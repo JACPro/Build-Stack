d4f5cc592ba0e2650ecccdbbc6878776a22837e3
JACPro <jamesacpro@gmail.com>
2021-04-16 10:20:02 +0100

    Initial commit

---
+++ Assets/Scripts/MovingCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * moveSpeed;
+    }
+}
